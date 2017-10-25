3fe2dafdf653911fc48d7f886734d9b56cd69302
Verhoef <c.verhoef@student.tue.nl>
2017-10-25 23:33:13 +0200

    Implemented test graph

--- src/org/processmining/streamsocialnetworks/louvain/visualizers/LSocialNetworkVisualizer.java
+++ src/org/processmining/streamsocialnetworks/louvain/visualizers/LSocialNetworkVisualizer.java
@@ -1,5 +1,9 @@
 package org.processmining.streamsocialnetworks.louvain.visualizers;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
 
@@ -9,6 +13,11 @@ import org.processmining.framework.plugin.PluginContext;
 import org.processmining.framework.plugin.annotations.Plugin;
 import org.processmining.framework.plugin.annotations.PluginVariant;
 import org.processmining.streamsocialnetworks.louvain.LSocialNetwork;
+
+import org.graphstream.graph.*;
+import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.ui.swingViewer.ViewPanel;
+import org.graphstream.ui.view.Viewer;
 
 import com.fluxicon.slickerbox.factory.SlickerFactory;
 
@@ -26,18 +35,36 @@ public class LSocialNetworkVisualizer {
 
 	public static JComponent visualize(final LSocialNetwork network) {
 		JComponent panel = new JPanel();
+		
 		switch (network.getNetworkType()) {
+			case SIMILAR_TASK :
+				// transform the input network to visualization library interface, e.g. graphstream
+				// visualize undirected edges;
+				// return the visualization
+				// panel.add(SlickerFactory.instance().createLabel("This should become an undirected graph."));		
+				Graph graph = new SingleGraph("Tutorial 1");
+				
+				graph.addNode("A" );
+				graph.addNode("B" );
+				graph.addNode("C" );
+				graph.addEdge("AB", "A", "B");
+				graph.addEdge("BC", "B", "C");
+				graph.addEdge("CA", "C", "A");
+				
+				Viewer viewer = new Viewer(graph, Viewer.ThreadingModel.GRAPH_IN_ANOTHER_THREAD);
+				ViewPanel viewPanel = viewer.addDefaultView(false);
+				viewPanel.setOpaque(false);
+		
+				panel.setLayout(new BorderLayout());
+		        panel.add(viewPanel, BorderLayout.CENTER);
+		        viewer.enableAutoLayout();
+     
+				break;
 			case HANDOVER :
 			case WORKING_TOGETHER :
 				panel.add(SlickerFactory.instance().createLabel("This should become a directed graph."));
-				// transfrom the input network to visualization library interface, e.g. graphstream
+				// transform the input network to visualization library interface, e.g. graphstream
 				// visualize directed edges;
-				// return the visualization
-				break;
-			case SIMILAR_TASK :
-				panel.add(SlickerFactory.instance().createLabel("This should become an undirected graph."));
-				// transfrom the input network to visualization library interface, e.g. graphstream
-				// visualize undirected edges;
 				// return the visualization
 				break;
 		}
